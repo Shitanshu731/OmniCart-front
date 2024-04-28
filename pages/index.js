4c6ce178fb8c2cff2587e2e20321960d179dfe9e
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-28 21:02:36 +0530

    Build Check

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
-import { mongooseConnect } from "@/lib/mongoose";
-import { Product } from "@/models/Product";
 
-export default function Homepage({product}) {
-  console.log(product);
+export default function Homepage() {
+  // console.log(product);
   return (
     <div>
       <Header />
-      <Featured product={product} />
+      <Featured />
     </div>
   )
 }
 
-export async function getServerSideProps(){
-    const featuredProductId = '662e5cd3d5c6b70d99152155';
-    await mongooseConnect();
-    const product = await Product.findById(featuredProductId);
-    return {
-      props : {product : JSON.parse(JSON.stringify(product))}
-    }
+// export async function getServerSideProps(){
+//     const featuredProductId = '662e5cd3d5c6b70d99152155';
+//     await mongooseConnect();
+//     const product = await Product.findById(featuredProductId);
+//     return {
+//       props : {product : JSON.parse(JSON.stringify(product))}
+//     }
 
-}
+// }
