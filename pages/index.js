b3ad67f62bb9e0d98c46722c1ccde6aa14e328c5
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-29 11:59:48 +0530

    Fix hydration err

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import NewProducts from "@/components/NewProducts";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/product";
 
@@ -9,6 +10,7 @@ export default function Homepage({featuredProduct,newProducts}) {
     <div>
       <Header />
       <Featured product={featuredProduct} />
+      <NewProducts />
     </div>
   )
 }
