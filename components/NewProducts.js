cb5a4beff6dd4c76c2b5e546169d1387a668d0eb
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-29 13:33:53 +0530

    Add Products

--- components/NewProducts.js
+++ components/NewProducts.js
@@ -3,14 +3,14 @@ import ProductBox from "./ProductBox";
 
 export default function NewProducts({products}){
     return (
-        <Center>
-            <div className="grid grid-cols-3">
+        <div className="mx-auto w-[70%] mt-2 ">
+            <div className="grid grid-cols-4 gap-5 max-md:grid-cols-2">
                 {products?.length > 0 && products.map(product => (
                     <div key={product.id}>
                     <ProductBox {...product} />
                     </div>
                 )) }
             </div>
-        </Center>
+        </div >
     )
 }
--- components/ProductBox.js
+++ components/ProductBox.js
@@ -1,7 +1,31 @@
-export default function ProductBox({id, title, description, price}){
-    return(
-        <div>
-            {title}
+export default function ProductBox({ id, title, description, images, price }) {
+  return (
+    <div className="">
+      <div className="gap-5 flex">
+        <div className="border-2 border-slate-200 p-10 rounded-lg   w-[270px] h-[150px] items-center flex justify-center shadow-lg  bg-slate-300 ">
+          <img className="w-[70%]" src={images[0]} alt={title} />
         </div>
-    )
+      </div>
+      <h1 className=" font-bold text-lg">{title}</h1>
+      <div className="flex justify-between items-center">
+      <h2 className="font-bold ">₹{price}</h2>
+      <button className="bg-blue-500 border-2 shadow-lg  rounded-sm p-2 flex gap-[10px] ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+          />
+        </svg>
+      </button>
+      </div>
+    </div>
+  );
 }
