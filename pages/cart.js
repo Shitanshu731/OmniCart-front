7edd3285ce95ba919d823e7ec0c3d8780f7d9f43
Shitcoder731 <shitanshu.731@gmail.com>
2024-05-15 13:06:48 +0530

    Add eventlistenes for the address inputs

--- pages/cart.js
+++ pages/cart.js
@@ -7,6 +7,13 @@ import CountUp from "react-countup";
 export default function CartPage() {
   const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
   const [products, setProducts] = useState([]);
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [city, setCity] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [address2, setAddress2] = useState("");
+  const [country, setCountry] = useState("");
   useEffect(() => {
     if (cartProducts.length > 0) {
       axios.post("/api/cart", { ids: cartProducts }).then((response) => {
@@ -96,38 +103,52 @@ export default function CartPage() {
           <div className="gap-4 bg-slate-200 rounded-[10px] p-[30px] flex flex-col">
             <h2>Order Information</h2>
             <input
+              onChange={(e) => setAddress(e.target.value)}
+              value={address}
               className="outline-none p-2 rounded-sm shadow-lg"
               type="text"
               placeholder="Address"
             />
             <input
+              onChange={(e) => setAddress2(e.target.value)}
+              value={address2}
               className="outline-none p-2 rounded-sm shadow-lg"
               type="text"
               placeholder="Address2"
             />
             <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className="outline-none p-2 rounded-sm shadow-lg"
               type="email"
               placeholder="email"
             />
             <div className="flex gap-4">
               <input
+                onChange={(e) => setCity(e.target.value)}
+                value={city}
                 className=" w-[50%] outline-none p-2 rounded-sm shadow-lg"
                 type="text"
                 placeholder="City"
               />
               <input
+                onChange={(e) => setPostalCode(e.target.value)}
+                value={postalCode}
                 className="w-[50%] outline-none p-2 rounded-sm shadow-lg"
                 type="text"
                 placeholder="Postal Code"
               />
             </div>
             <input
+              onChange={(e) => setStreetAddress(e.target.value)}
+              value={streetAddress}
               className="outline-none p-2 rounded-sm shadow-lg"
               type="text"
               placeholder="Street Address"
             />
             <input
+              onChange={(e) => setCountry(e.target.value)}
+              value={country}
               className="outline-none p-2 rounded-sm shadow-lg"
               type="text"
               placeholder="Country"
